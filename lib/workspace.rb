0fc3025080d9a4e5fc22436fea85587b921f7569
Devin Helmgren <devincollege@gmail.com>
2020-03-12 20:27:03 -0700

    selecting a user

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -11,9 +11,33 @@ class Workspace
   end
 
   def select_channel
+
+    search_term = gets.chomp
+
+    channels.each do |channel|
+      if channel.name == search_term || channel.slack_id == search_term
+        selected = channel
+        return "Okay, #{selected.name} has been selected" 
+      end
+    end
+
+    selected = nil
+    return "Sorry, I couldn't find that channel."
   end
 
   def select_user
+
+    search_term = gets.chomp
+
+    users.each do |user|
+      if user.name == search_term || user.slack_id == search_term
+        selected = user
+        return "Okay, #{selected.name} has been selected" 
+      end
+    end
+
+    selected = nil
+    return "Sorry, I couldn't find that user."
   end
 
   def show_details
