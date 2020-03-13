7b548f4db4dac536d339f53f4e29ba047785d605
Devin Helmgren <devincollege@gmail.com>
2020-03-12 20:12:38 -0700

    channel tests green

--- lib/channel.rb
+++ lib/channel.rb
@@ -5,7 +5,7 @@ class Channel < Recipient
   attr_reader :topic, :member_count
 
   def initialize( topic:, member_count:, name:, slack_id:)
-    super(slack_id, name)
+    super(slack_id: slack_id, name: name)
     @topic = topic
     @member_count = member_count
   end
@@ -17,6 +17,24 @@ class Channel < Recipient
 
   #api endpoint https://api.slack.com/methods/conversations.list
   def self.list_all
+    #get the data
+    response = Channel.get("https://slack.com/api/conversations.list")
+
+    #parse the data, instansiate an object
+    channels = []
+
+    response["channels"].each do |item|
+      channels << Channel.new(
+                name: item["name"],
+                slack_id: item["id"],
+                topic: item["topic"]["value"],
+                member_count: item["num_members"]
+      )
+    end
+
+    #return the list of instansiated objects
+    #this format tells me that I can use this to instansiate a Workspace!
+    return channels
   end
 
 end
