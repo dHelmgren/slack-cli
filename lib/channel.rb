036b7031f805cbb04814b0bbae51601355b3d473
Devin Helmgren <devincollege@gmail.com>
2020-03-12 20:41:18 -0700

    hindsight

--- lib/channel.rb
+++ lib/channel.rb
@@ -5,7 +5,7 @@ class Channel < Recipient
   attr_reader :topic, :member_count
 
   def initialize( topic:, member_count:, name:, slack_id:)
-    super(slack_id, name)
+    super(slack_id: slack_id, name: name)
     @topic = topic
     @member_count = member_count
   end
--- lib/user.rb
+++ lib/user.rb
@@ -4,7 +4,7 @@ class User < Recipient
   attr_reader :real_name, :status_text, :status_emoji
 
   def initialize( real_name:, status_text:, status_emoji:, name:, slack_id:)
-    super(slack_id, name)
+    super(slack_id: slack_id, name: name)
 
     @real_name = real_name
     @status_text = status_text
